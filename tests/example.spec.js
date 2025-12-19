963a28573361e28a716018b0466b5e24629e3cfc
jairo jean <jairojean.br@gmail.com>
2025-12-19 16:10:04 -0300

    teste login

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,2 +1,8 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+
+test('Teste de login com sucesso.', async ({ page}) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.locator('[data-test= "username"]').fill('standard_user')
+    await page.locator('[data-test= "password"]').fill('secret_sauce')
+})
